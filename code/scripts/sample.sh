cd9fa755cee07ed446b2321fb95d2b2b1a3bfdd1
Rahul Sankar <rahulsnkr@gmail.com>
2022-12-10 17:21:05 -0500

    add transformer cmd args

--- code/scripts/sample.sh
+++ code/scripts/sample.sh
@@ -5,9 +5,13 @@ cfg=$1
 imgs_per_sent=16
 cuda=True
 gpu_id=0
+use_transformer=True
+transformer_type='roberta'
 
 python src/sample.py \
         --cfg $cfg \
         --imgs_per_sent $imgs_per_sent \
         --cuda $cuda \
         --gpu_id $gpu_id \
+        --use_transformer  $use_transformer \
+        --transformer_type $transformer_type
